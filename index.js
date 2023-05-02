5ab670dc38c769c17e343d20849c8cbab8dc8ab3
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-05-02 22:57:54 +0600

    chef data added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const chefsData = require('./data/chefsData.json')
+const cors = require('cors')
 const app = express()
 const port = 3000
+app.use(cors())
 
 app.get('/',(req,res)=>{
-    res.send('Chef Recipe Hunter server is running')
+    res.send('Chef Recipe Hunter server is running');
+})
+app.get('/chefsData',(req,res)=>{
+    res.send(chefsData);
 })
 
 app.listen(port,()=>{
