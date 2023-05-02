3ad760dc7958100abd9ce2dce2c97f4d8536d3ad
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-05-03 01:29:00 +0600

    recipe data added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const chefsData = require('./data/chefsData.json')
+const recipes = require('./data/recipes.json')
 const cors = require('cors')
 const app = express()
 const port = 3000
@@ -10,6 +11,14 @@ app.get('/',(req,res)=>{
 })
 app.get('/chefsData',(req,res)=>{
     res.send(chefsData);
+})
+app.get('/recipes',(req,res)=>{
+    res.send(recipes);
+})
+app.get('/recipes/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const chefRecipes = recipes.filter(recipe=> parseInt(recipe.chef_id) === id);
+    res.send(chefRecipes)
 })
 
 app.listen(port,()=>{
